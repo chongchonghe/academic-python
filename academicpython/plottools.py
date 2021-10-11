2e36409da9dd1399fc6bb7468e37ecc2d51c08d9
ChongChong He <he.cc.phy@gmail.com>
2021-10-11 12:20:46 -0400

    mac

--- academicpython/plottools.py
+++ academicpython/plottools.py
@@ -16,6 +16,7 @@ turn it on in case you want to enable overwriting ocasionally. e.g.
 
 import os
 import __main__
+import math
 import numpy as np
 from math import atan, pi
 import matplotlib
@@ -145,13 +146,45 @@ def scaled_figure(rows=1, columns=1, **kwargs):
     return f, ax
 
 def sized_figure(rows=1, columns=1, mergex=True, mergey=True,
-                     rescale=1.0, top=False, right=False,
-                     ret_size=False, figsize=None,
-                     **kwargs):
+                 rescale=1.0, top=False, right=False,
+                 ret_size=False, figsize=None,
+                 **kwargs):
 
     ratio = 0.68
     w_inch = 3.6 * rescale
     h_inch = w_inch * ratio
+    l_inch = 0.68
+    r_inch = 0.14 if not right else l_inch
+    b_inch = 0.532
+    t_inch = 0.14 if not top else b_inch
+    fx = columns * w_inch + l_inch + r_inch
+    fy = rows * h_inch + b_inch + t_inch
+    if not mergex:
+        fy += (rows - 1) * (b_inch + t_inch)
+    if ret_size:
+        return [fx, fy]
+    if figsize is None:
+        figsize = [fx, fy]
+    f, ax = plt.subplots(rows, columns, figsize=figsize, **kwargs)
+    plt.subplots_adjust(left=l_inch/fx, right=1-r_inch/fx,
+                    bottom=b_inch/fy, top=1-t_inch/fy,
+                    wspace=0,
+                    hspace=0 if mergex else (b_inch + t_inch) / h_inch,
+    )
+    return f, ax
+
+
+def sized_figure_sci(rows=1, columns=1, mergex=True, mergey=True,
+                 rescale=1.0, top=False, right=False,
+                 ret_size=False, figsize=None,
+                 **kwargs):
+
+    f = plt.figure()
+    w_inch, h_inch = fig.get_size_inches()
+
+    # ratio = 0.68
+    # w_inch = 3.6 * rescale
+    # h_inch = w_inch * ratio
     l_inch = 0.68
     r_inch = 0.14 if not right else l_inch
     b_inch = 0.532
@@ -376,21 +409,6 @@ def setMyLogFormat(ax, axis='y'):
         ax.xaxis.set_major_formatter(ticker.FuncFormatter(myLogFormat))
     if axis in ['y', 'both']:
         ax.yaxis.set_major_formatter(ticker.FuncFormatter(myLogFormat))
-
-def plot_ind_cbar(ax=None, fn="colorbar.pdf", cmap='viridis', lims=[0, 1], label=''):
-    """ Plot a individual colorbar, for demonstration of the color scale """
-
-    import plotutils as pu
-    if ax is None or ax == 'new':
-        f, axhid = sized_figure(figsize=[4, 1])
-        axhid.axis('off')
-        ax = f.add_axes([.1, .5, .8, .4])
-    ax0 = f.add_axes([-1, -1, .1, .1])
-    im = ax0.imshow([lims], cmap=cmap)
-    cb = plt.colorbar(im, cax=ax, orientation='horizontal')
-    cb.set_label(label)
-    plt.savefig(fn)
-    return
 
 def plot_ind_cbar(ax=None, cmap='viridis', lims=[0, 1], label='',
                   is_use_plotutils=False):
@@ -630,3 +648,166 @@ def mylegend(ax=None, loc=0.3, kind='top', align=True, nslope=1,
         # increase xlim by 10%
         ax.set_xlim(xmin, xmax + margin_right * (xmax - xmin))
 
+class CurvedText(matplotlib.text.Text):
+    """
+    A text object that follows an arbitrary curve.
+
+    https://stackoverflow.com/questions/19353576/curved-text-rendering-in-matplotlib
+    """
+    def __init__(self, x, y, text, axes, **kwargs):
+        if '$' in text:
+            print("Warning: CurvedText does not support LaTeX.")
+            return
+        super(CurvedText, self).__init__(x[0],y[0],' ', **kwargs)
+
+        axes.add_artist(self)
+
+        ##saving the curve:
+        self.__x = x
+        self.__y = y
+        self.__zorder = self.get_zorder()
+
+        ##creating the text objects
+        self.__Characters = []
+        for c in text:
+            if c == ' ':
+                ##make this an invisible 'a':
+                t = matplotlib.text.Text(0,0,'a')
+                t.set_alpha(0.0)
+            else:
+                t = matplotlib.text.Text(0,0,c, **kwargs)
+
+            #resetting unnecessary arguments
+            t.set_ha('center')
+            t.set_rotation(0)
+            t.set_zorder(self.__zorder +1)
+
+            self.__Characters.append((c,t))
+            axes.add_artist(t)
+
+
+    ##overloading some member functions, to assure correct functionality
+    ##on update
+    def set_zorder(self, zorder):
+        super(CurvedText, self).set_zorder(zorder)
+        self.__zorder = self.get_zorder()
+        for c,t in self.__Characters:
+            t.set_zorder(self.__zorder+1)
+
+    def draw(self, renderer, *args, **kwargs):
+        """
+        Overload of the Text.draw() function. Do not do
+        do any drawing, but update the positions and rotation
+        angles of self.__Characters.
+        """
+        self.update_positions(renderer)
+
+    def update_positions(self,renderer):
+        """
+        Update positions and rotations of the individual text elements.
+        """
+
+        #preparations
+
+        ##determining the aspect ratio:
+        ##from https://stackoverflow.com/a/42014041/2454357
+
+        ##data limits
+        xlim = self.axes.get_xlim()
+        ylim = self.axes.get_ylim()
+        ## Axis size on figure
+        figW, figH = self.axes.get_figure().get_size_inches()
+        ## Ratio of display units
+        _, _, w, h = self.axes.get_position().bounds
+        ##final aspect ratio
+        aspect = ((figW * w)/(figH * h))*(ylim[1]-ylim[0])/(xlim[1]-xlim[0])
+
+        #points of the curve in figure coordinates:
+        x_fig,y_fig = (
+            np.array(l) for l in zip(*self.axes.transData.transform([
+            (i,j) for i,j in zip(self.__x,self.__y)
+            ]))
+        )
+
+        #point distances in figure coordinates
+        x_fig_dist = (x_fig[1:]-x_fig[:-1])
+        y_fig_dist = (y_fig[1:]-y_fig[:-1])
+        r_fig_dist = np.sqrt(x_fig_dist**2+y_fig_dist**2)
+
+        #arc length in figure coordinates
+        l_fig = np.insert(np.cumsum(r_fig_dist),0,0)
+
+        #angles in figure coordinates
+        rads = np.arctan2((y_fig[1:] - y_fig[:-1]),(x_fig[1:] - x_fig[:-1]))
+        degs = np.rad2deg(rads)
+
+
+        rel_pos = 10
+        for c,t in self.__Characters:
+            #finding the width of c:
+            t.set_rotation(0)
+            t.set_va('center')
+            bbox1  = t.get_window_extent(renderer=renderer)
+            w = bbox1.width
+            h = bbox1.height
+
+            #ignore all letters that don't fit:
+            if rel_pos+w/2 > l_fig[-1]:
+                t.set_alpha(0.0)
+                rel_pos += w
+                continue
+
+            elif c != ' ':
+                t.set_alpha(1.0)
+
+            #finding the two data points between which the horizontal
+            #center point of the character will be situated
+            #left and right indices:
+            il = np.where(rel_pos+w/2 >= l_fig)[0][-1]
+            ir = np.where(rel_pos+w/2 <= l_fig)[0][0]
+
+            #if we exactly hit a data point:
+            if ir == il:
+                ir += 1
+
+            #how much of the letter width was needed to find il:
+            used = l_fig[il]-rel_pos
+            rel_pos = l_fig[il]
+
+            #relative distance between il and ir where the center
+            #of the character will be
+            fraction = (w/2-used)/r_fig_dist[il]
+
+            ##setting the character position in data coordinates:
+            ##interpolate between the two points:
+            x = self.__x[il]+fraction*(self.__x[ir]-self.__x[il])
+            y = self.__y[il]+fraction*(self.__y[ir]-self.__y[il])
+
+            #getting the offset when setting correct vertical alignment
+            #in data coordinates
+            t.set_va(self.get_va())
+            bbox2  = t.get_window_extent(renderer=renderer)
+
+            bbox1d = self.axes.transData.inverted().transform(bbox1)
+            bbox2d = self.axes.transData.inverted().transform(bbox2)
+            dr = np.array(bbox2d[0]-bbox1d[0])
+
+            #the rotation/stretch matrix
+            rad = rads[il]
+            rot_mat = np.array([
+                [math.cos(rad), math.sin(rad)*aspect],
+                [-math.sin(rad)/aspect, math.cos(rad)]
+            ])
+
+            ##computing the offset vector of the rotated character
+            drp = np.dot(dr,rot_mat)
+
+            #setting final position and rotation:
+            t.set_position(np.array([x,y])+drp)
+            t.set_rotation(degs[il])
+
+            t.set_va('center')
+            t.set_ha('center')
+
+            #updating rel_pos to right edge of character
+            rel_pos += w-used
